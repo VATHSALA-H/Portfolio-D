98c8b63f28d27ce71f2e06eebcc9e7dfdb574036
VATHSALA-H <vathsalahariharan@gmail.com>
2025-01-27 09:26:18 +0530

    1st commit

---
+++ components/Contact.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import bgimage from '../assets/footer-bg-color.png'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+import { useState } from 'react'
+import { motion } from "motion/react"
+
+const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b47c9b62-c8a3-438a-bb3a-a00cb1d81ef3");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }} id='contact' className='w-full px-[12%] py-10 scroll-mt-20' style={{
+        backgroundImage: `url(${require('../assets/footer-bg-color.png')})`, // Background image applied here
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}>
+      <motion.h4 initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }} className="text-center mb-2 text-lg font-Ovo">Connect with me</motion.h4>
+      <motion.h2 initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }} className="text-center text-5xl font-Ovo">Get in touch</motion.h2>
+      <motion.p initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }} className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        i would love to hear from you!.If you have any questions,comments, or feedback, please use the form below.
+      </motion.p>
+      <motion.form
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }} onSubmit={onSubmit} className='max-w-2xl mx-auto'>
+        <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
+
+          <motion.input initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.1 }} type="text" placeholder='Enter your name' required
+            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name' />
+
+          <motion.input initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.1 }} type='email' placeholder='Enter your email' required
+            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email' />
+        </div>
+        <textarea rows='6' placeholder='Enter your message' required
+          className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'></textarea>
+        <button type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'>Submit now
+          <Image src={assets.right_arrow_white} alt='' className='w-4' />
+        </button>
+
+        <p className='mt-4'>{result}</p>
+      </motion.form>
+    </motion.div>
+  )
+}
+
+export default Contact
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+          lightHover:'#fcf4ff',
+          darkHover:'#2a004a',
+          darkTheme:'#11001F',
       },
+      fontFamily:{
+        Outfit:["Outfit","sans-serif"],
+        Ovo:["Ovo","serif"]
+      },
+      screens: {
+        'c-lg': '1440px',
+        'c-md': '680px',
+      },
+      boxShadow:{
+        'black' : '4px 4px 0 #000',
+        'white':'4px 4px 0 #fff'
+      }
     },
   },
+  darkMode:'selector',
   plugins: [],
 } satisfies Config;
